88f9aa29294311a824ac6e658eee5897c22debe2
zhaobao <zhaobao@tiansishangwu.com>
2018-12-23 03:09:00 +0800

    配置eureka服务器

--- src/main/java/com/zhao/finder/FinderApplication.java
+++ src/main/java/com/zhao/finder/FinderApplication.java
@@ -3,9 +3,11 @@ package com.zhao.finder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @Slf4j
+@EnableEurekaServer
 public class FinderApplication {
     public static void main(String[] args) {
         SpringApplication.run(FinderApplication.class, args);
